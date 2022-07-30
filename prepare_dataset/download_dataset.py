0806af90333f4b5e5d47dff72bbcd43cac358c21
Mohib Rehman <mohibrehman31@gmail.com>
2022-07-30 23:26:37 +0000

    m

--- prepare_dataset/download_dataset.py
+++ prepare_dataset/download_dataset.py
@@ -22,27 +22,9 @@ if __name__ == '__main__':
     make_folder(train_annotation_dir)
     make_folder(test_video_dir)
     make_folder(test_annotation_dir)
+    
+    wget.download('https://lab.osai.ai/datasets/openttgames/data/game_1.mp4',train_video_dir)
+    wget.download('https://lab.osai.ai/datasets/openttgames/data/test_1.mp4',test_video_dir)
+    wget.download('https://lab.osai.ai/datasets/openttgames/data/test_1.zip',train_annotation_dir)
+    wget.download('https://lab.osai.ai/datasets/openttgames/data/test_1.zip',test_annotation_dir)
 
-    common_url = 'https://lab.osai.ai/datasets/openttgames/data/'
-
-    train_video_filenames = ['game_{}.mp4'.format(i) for i in range(1)]  # 1 to 5
-    train_annotation_filenames = ['game_{}.zip'.format(i) for i in range(1)]  # 1 to 5
-
-    test_video_filenames = ['test_{}.mp4'.format(i) for i in range(1)]  # 1 to 7
-    test_annotation_filenames = ['test_{}.zip'.format(i) for i in range(1)]  # 1 to 7
-
-    for video_fn, annos_fn in zip(train_video_filenames, train_annotation_filenames):
-        if not os.path.isfile(os.path.join(train_video_dir, video_fn)):
-            print('Downloading...{}'.format(common_url + video_fn))
-            wget.download(common_url + video_fn, os.path.join(train_video_dir, video_fn))
-        if not os.path.isfile(os.path.join(train_annotation_dir, annos_fn)):
-            print('Downloading...{}'.format(common_url + annos_fn))
-            wget.download(common_url + annos_fn, os.path.join(train_annotation_dir, annos_fn))
-
-    for video_fn, annos_fn in zip(test_video_filenames, test_annotation_filenames):
-        if not os.path.isfile(os.path.join(test_video_dir, video_fn)):
-            print('Downloading...{}'.format(common_url + video_fn))
-            wget.download(common_url + video_fn, os.path.join(test_video_dir, video_fn))
-        if not os.path.isfile(os.path.join(test_annotation_dir, annos_fn)):
-            print('Downloading...{}'.format(common_url + annos_fn))
-            wget.download(common_url + annos_fn, os.path.join(test_annotation_dir, annos_fn))
